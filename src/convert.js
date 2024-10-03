4d84402000aeb00466834b970ecdb540c3c11945
Lord-McSweeney <Lord-McSweeney@github.com>
2024-10-03 11:04:34 -0700

    convert/main: Separate project conversion code from SB3 file reading code

---
+++ src/convert.js
@@ -0,0 +1,151 @@
+const { sanitizeString } = require("./utils.js");
+const { debug, info, warn, error, fatal } = require("./logging.js");
+const { processBlock, processToplevelBlocks } = require("./blockProcessor.js");
+
+function convertStageTarget(targetInfo) {
+    let runnableCode = "";
+
+    let toplevelBlocks = [];
+    for (let i in targetInfo.blocks) {
+        if (targetInfo.blocks[i].topLevel) {
+            toplevelBlocks.push(targetInfo.blocks[i]);
+        }
+    }
+
+    // Parse top-level blocks
+    runnableCode += processToplevelBlocks(targetInfo.blocks, toplevelBlocks);
+
+    let code = "";
+
+    // Initialize global variables
+    const variables = targetInfo.variables;
+    for (let i in variables) {
+        const variable = variables[i];
+
+        // TODO does this even matter? Are all variables just strings?
+        let realString = null;
+        if (typeof variable[1] === "number") {
+            realString = variable[1].toString();
+        } else {
+            realString = "\"" + sanitizeString(variable[1]) + "\"";
+        }
+
+        code += `
+                globalVariables.set("${sanitizeString(i)}", ${realString});
+`;
+    }
+
+    // Initialize global lists
+    const lists = targetInfo.lists;
+    for (let i in lists) {
+        const list = lists[i];
+
+        code += `
+                globalLists.set("${sanitizeString(i)}", ${JSON.stringify(list[1])});
+`;
+    }
+
+    // Finally, create stage constructor
+    code += `
+                targets.push({
+                    ctor: await createSpriteConstructor(
+                        "${sanitizeString(targetInfo.name)}",
+                        240,
+                        180,
+                        100,
+                        0,
+                        ${JSON.stringify(targetInfo.costumes)},
+                        ${targetInfo.currentCostume},
+                        "all around",
+                        {},
+                        {},
+                        true,
+                        function(isClone) {
+${runnableCode}
+                        }
+                    ),
+                    visible: true,
+                });
+`;
+
+    return code;
+}
+
+function convertTarget(targetInfo) {
+    let runnableCode = "";
+
+    let toplevelBlocks = [];
+    for (let i in targetInfo.blocks) {
+        if (targetInfo.blocks[i].topLevel) {
+            toplevelBlocks.push(targetInfo.blocks[i]);
+        }
+    }
+
+    // Parse top-level blocks
+    runnableCode += processToplevelBlocks(targetInfo.blocks, toplevelBlocks);
+
+    // Initialize local variables
+    const variables = targetInfo.variables;
+    const resultVariables = {};
+    for (let i in variables) {
+        resultVariables[i] = variables[i][1];
+    }
+
+    // Initialize local lists
+    const lists = targetInfo.lists;
+    const resultLists = {};
+    for (let i in lists) {
+        resultLists[i] = lists[i][1];
+    }
+
+    // Create sprite constructor
+    let code = `
+                targets.push({
+                    ctor: await createSpriteConstructor(
+                        "${sanitizeString(targetInfo.name)}",
+                        ${240 + targetInfo.x},
+                        ${180 - targetInfo.y},
+                        ${targetInfo.size},
+                        ${targetInfo.direction - 90},
+                        ${JSON.stringify(targetInfo.costumes)},
+                        ${targetInfo.currentCostume},
+                        "${sanitizeString(targetInfo.rotationStyle)}",
+                        ${JSON.stringify(resultVariables)},
+                        ${JSON.stringify(resultLists)},
+                        false,
+                        function(isClone) {
+${runnableCode}
+                        }
+                    ),
+                    visible: ${targetInfo.visible},
+                });
+`;
+
+    return code;
+}
+
+function convertProject(projectData, wrapperFile) {
+    const data = JSON.parse(projectData);
+
+    let code = "";
+    let targets = data.targets;
+    let encounteredStageTarget = false;
+    for (let i in targets) {
+        if (targets[i].isStage) {
+            if (encounteredStageTarget) {
+                fatal("Duplicate stage target");
+                return "";
+            }
+
+            code += convertStageTarget(targets[i]);
+            encounteredStageTarget = true;
+        } else {
+            code += convertTarget(targets[i]);
+        }
+    }
+
+    // DO NOT USE String.prototype.replace HERE- SPECIAL $ REPLACEMENT CHARACTERS IN `code` MUST BE SANITIZED
+    return wrapperFile.toString().split("##code##").join(code);
+}
+
+module.exports = { convertProject };
--- src/main.js
+++ src/main.js
@@ -20,157 +20,8 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const path = require('path');
 
-const { sanitizeString } = require("./utils.js");
 const { debug, info, warn, error, fatal } = require("./logging.js");
-const { processBlock, processToplevelBlocks } = require("./blockProcessor.js");
-
-const assetPath = path.join(__dirname, "assets/");
-
-function convertStageTarget(targetInfo) {
-    let runnableCode = "";
-
-    let toplevelBlocks = [];
-    for (let i in targetInfo.blocks) {
-        if (targetInfo.blocks[i].topLevel) {
-            toplevelBlocks.push(targetInfo.blocks[i]);
-        }
-    }
-
-    // Parse top-level blocks
-    runnableCode += processToplevelBlocks(targetInfo.blocks, toplevelBlocks);
-
-    let code = "";
-
-    // Initialize global variables
-    const variables = targetInfo.variables;
-    for (let i in variables) {
-        const variable = variables[i];
-
-        // TODO does this even matter? Are all variables just strings?
-        let realString = null;
-        if (typeof variable[1] === "number") {
-            realString = variable[1].toString();
-        } else {
-            realString = "\"" + sanitizeString(variable[1]) + "\"";
-        }
-
-        code += `
-                globalVariables.set("${sanitizeString(i)}", ${realString});
-`;
-    }
-
-    // Initialize global lists
-    const lists = targetInfo.lists;
-    for (let i in lists) {
-        const list = lists[i];
-
-        code += `
-                globalLists.set("${sanitizeString(i)}", ${JSON.stringify(list[1])});
-`;
-    }
-
-    // Finally, create stage constructor
-    code += `
-                targets.push({
-                    ctor: await createSpriteConstructor(
-                        "${sanitizeString(targetInfo.name)}",
-                        240,
-                        180,
-                        100,
-                        0,
-                        ${JSON.stringify(targetInfo.costumes)},
-                        ${targetInfo.currentCostume},
-                        "all around",
-                        {},
-                        {},
-                        true,
-                        function(isClone) {
-${runnableCode}
-                        }
-                    ),
-                    visible: true,
-                });
-`;
-
-    return code;
-}
-
-function convertTarget(targetInfo) {
-    let runnableCode = "";
-
-    let toplevelBlocks = [];
-    for (let i in targetInfo.blocks) {
-        if (targetInfo.blocks[i].topLevel) {
-            toplevelBlocks.push(targetInfo.blocks[i]);
-        }
-    }
-
-    // Parse top-level blocks
-    runnableCode += processToplevelBlocks(targetInfo.blocks, toplevelBlocks);
-
-    // Initialize local variables
-    const variables = targetInfo.variables;
-    const resultVariables = {};
-    for (let i in variables) {
-        resultVariables[i] = variables[i][1];
-    }
-
-    // Initialize local lists
-    const lists = targetInfo.lists;
-    const resultLists = {};
-    for (let i in lists) {
-        resultLists[i] = lists[i][1];
-    }
-
-    // Create sprite constructor
-    let code = `
-                targets.push({
-                    ctor: await createSpriteConstructor(
-                        "${sanitizeString(targetInfo.name)}",
-                        ${240 + targetInfo.x},
-                        ${180 - targetInfo.y},
-                        ${targetInfo.size},
-                        ${targetInfo.direction - 90},
-                        ${JSON.stringify(targetInfo.costumes)},
-                        ${targetInfo.currentCostume},
-                        "${sanitizeString(targetInfo.rotationStyle)}",
-                        ${JSON.stringify(resultVariables)},
-                        ${JSON.stringify(resultLists)},
-                        false,
-                        function(isClone) {
-${runnableCode}
-                        }
-                    ),
-                    visible: ${targetInfo.visible},
-                });
-`;
-
-    return code;
-}
-
-function convertProject(projectData) {
-    const data = JSON.parse(projectData);
-
-    let code = "";
-    let targets = data.targets;
-    let encounteredStageTarget = false;
-    for (let i in targets) {
-        if (targets[i].isStage) {
-            if (encounteredStageTarget) {
-                fatal("Duplicate stage target");
-                return "";
-            }
-
-            code += convertStageTarget(targets[i]);
-            encounteredStageTarget = true;
-        } else {
-            code += convertTarget(targets[i]);
-        }
-    }
-
-    // DO NOT USE String.prototype.replace HERE- SPECIAL $ REPLACEMENT CHARACTERS MUST BE SANITIZED
-    return fs.readFileSync(assetPath + "wrapper.html").toString().split("##code##").join(code);
-}
+const { convertProject } = require("./convert.js");
 
 async function main() {
     if (process.argv.length === 2) {
@@ -209,13 +60,17 @@ async function main() {
     fs.mkdirSync("./output/");
 
     try {
-        info("Reading extracted files from temp directory...");
+        // This operation shouldn't fail unless the asset file is missing
+        const assetPath = path.join(__dirname, "assets/");
+        const wrapperFile = fs.readFileSync(assetPath + "wrapper.html");
+
+        info("Reading extracted files from temp directory and converting project...");
         let files = fs.readdirSync("/tmp/scratch-extraction/");
         for (let i in files) {
             const file = files[i];
             const readData = fs.readFileSync("/tmp/scratch-extraction/" + file);
             if (file === "project.json") {
-                const result = convertProject(readData.toString("utf8"));
+                const result = convertProject(readData.toString("utf8"), wrapperFile);
                 fs.writeFileSync("./output/index.html", result);
             } else {
                 if (file.endsWith(".svg") || file.endsWith(".png") || file.endsWith(".wav")) {
